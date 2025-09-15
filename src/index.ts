a338ed9139a2d63c5040eca80085bc4912e4b923
Gabriel Melo <melogoncalvesbiel@gmail.com>
2025-09-15 10:49:25 -0300

    feature: create infos endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import { Elysia } from "elysia";
+import os from "node:os";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+app.get("/api/v1/details", () => {
+  return {
+    time: new Date().toLocaleDateString(),
+    hostname: os.hostname(),
+  };
+});
+
+app.get("/api/v1/healthz", () => {
+  return {
+    status: "ok",
+  };
+});
+
+app.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
